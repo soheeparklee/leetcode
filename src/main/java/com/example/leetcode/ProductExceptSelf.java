e8e9adcad563785b28324e2f7b2a83bcd0b991a1
soheeparklee <soheeparklee@gmail.com>
2025-05-30 17:23:13 +0200

    prefix, suffix in array

---
+++ src/main/java/com/example/leetcode/ProductExceptSelf.java
@@ -0,0 +1,20 @@
+package com.example.leetcode;
+
+public class ProductExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+
+        answer[0] = 1;
+        for(int i=1; i<length; i++){
+            answer[i] = answer[i-1] * nums[i-1];
+        }
+
+        int right = 1;
+        for(int i=length-1; i>=0; i--){
+            answer[i] = answer[i] * right;
+            right *= nums[i];
+        }
+        return answer;
+    }
+}
